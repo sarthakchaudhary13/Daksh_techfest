424f09b63955bf38e05688d94b2e6ba976253227
Sarthak Chaudhary <sarthakchaudhary13@gmail.com>
2019-04-03 01:38:10 +0530

    Create debbugingq4.c

---
+++ debbugingq4.c
@@ -0,0 +1,23 @@
+// Function to check whether the given
+// Returns 'true' if the string is a KEYWORD.
+bool isKeyword(char* str)
+{
+	if (!strcmp(str, "auto") || !strcmp(str, "default")
+		|| !strcmp(str, "signed") || !strcmp(str, "enum")
+		||!strcmp(str, "maloc") || !strcmp(str, "for")
+		|| !strcmp(str, "register") || !strcmp(str, "if")
+		|| !strcmp(str, "else") || !strcmp(str, "int")
+		|| !strcmp(str, "while") || !strcmp(str, "do")
+		|| !strcmp(str, "break") || !strcmp(str, "continue")
+		|| !strcmp(str, "double") || !strcmp(str, "float")
+		|| !strcmp(str, "return") || !strcmp(str, "char")
+		|| !strcmp(str, "case") || !strcmp(str, "const")
+		|| !strcmp(str, "sizeof") || !strcmp(str, "long")
+		|| !strcmp(str, "short") || !strcmp(str, "typedef")
+		|| !strcmp(str, "switch") || !strcmp(str, "unsigned")
+		|| !strcmp(str, "void") || !strcmp(str, "static")
+		|| !strcmp(str, "struct") || !strcmp(str, "goto")
+		|| !strcmp(str, "union") || !strcmp(str, "volatile"))
+		return (true);
+	return (false);
+}
